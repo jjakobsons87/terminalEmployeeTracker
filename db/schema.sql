44501ded0b04ec7dffce8031ed7a340b9331bd01
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-04-26 12:46:37 -0400

    update schema

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS empRole;
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+CREATE TABLE empRole (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INTEGER FOREIGN KEY
+);
+
+CREATE TABLE employee (
+    id INTEGER PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER FOREIGN KEY,
+    manager_id INTEGER
+);
